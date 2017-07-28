b3b2b638da9450447d9e4188ed13e6234c586b07
Ananth Kannan <kannan.ananth@gmail.com>
2017-07-28 18:01:08 -0500

    fist commit of a reference app

---
+++ src/test/java/com/mkyong/controller/BaseControllerTest.java
@@ -0,0 +1,15 @@
+package com.mkyong.controller;
+
+
+import org.junit.Test;
+import org.junit.Ignore;
+import static org.junit.Assert.assertEquals;
+
+public class BaseControllerTest {
+
+	@Test
+	public void welcomeName() 
+	{
+     	}
+
+}
